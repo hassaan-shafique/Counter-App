c97f4d5c031ebda37c63c5c3f79082811e5ab653
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-07 03:45:54 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+
 
 
 function App() {
+  const [num ,setNum] = useState(0);
  
+
+  const handleincrement = () =>{
+    setNum (num +1);
+  }
+  const handledecrement =() =>{
+    if (num> 0) {
+    setNum(num -1);
+    }
+    else{
+      alert("you have reached to the initial value");
+      setNum(0);
+    }
+  } 
+  const handlereset =() => {
+     
+      setNum (0);
+     
+  }
    
   return (
     <div className='main'>
     <h1 className='heading'>Counter App</h1>
       <div className="main-card">
         <div className="h1-part">
-          <h1>1</h1>
+          <h1>{num}</h1>
         </div>
         <div className="button-container">
-          <button className="green">Increment</button>
-          <button className="red">Decrement</button>
+          <button className="green" onClick={handleincrement}>Increment</button>
+          <button className="red" onClick={handledecrement}>Decrement</button>
         </div>
         <div className='reset'>
-        <button className='reset-1'> Reset</button> 
+        <button className='reset-1' onClick={handlereset}> Reset</button> 
         </div>
       </div>
     </div>
