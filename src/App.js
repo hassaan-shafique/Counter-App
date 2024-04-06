9244c574aa5f9df019bf8608dd605324386a922b
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-07 02:27:43 +0500

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+  const [num, setNum] = useState(0);
+  const initialCount =0;
+  
+  const handleincrement = () => {
+    setNum(num +1) ;
+  }
+  const handledecrement = () => {
+    if (num >0) {
+    setNum(num - 1);
+    }
+    else {
+      alert("you have reached to zero ");
+      setNum(0);
+      
+    }
+    
+    
+  };
+
+ const handlerest = () =>{
+ setNum (initialCount);
+ }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <p> {num}</p>
+        <div>
+          <button className="increment " onClick={handleincrement}>
+           
+            Increment
+          </button>
+          <button className="decrement " onClick={handledecrement}>
+           
+            Decrement
+          </button>
+          <button className="decrement " onClick={handlerest}>
+            
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
