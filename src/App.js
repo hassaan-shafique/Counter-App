9081f05309b3459edf4bdc284c5b09cb8c97044a
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-07 03:19:16 +0500

    counter style

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import './App.css';
-import { useState } from "react";
-
 
 
 function App() {
-  const [num, setNum] = useState(0);
-  const initialCount =0;
-  
-  const handleincrement = () => {
-    setNum(num +1) ;
-  }
-  const handledecrement = () => {
-    if (num >0) {
-    setNum(num - 1);
-    }
-    else {
-      alert("you have reached to zero ");
-      setNum(0);
-      
-    }
-    
-    
-  };
-
- const handlerest = () =>{
- setNum (initialCount);
- }
+ 
    
   return (
-    <div>
-      <div>
-        <p> {num}</p>
-        <div>
-          <button className="increment " onClick={handleincrement}>
-           
-            Increment
-          </button>
-          <button className="decrement " onClick={handledecrement}>
-           
-            Decrement
-          </button>
-          <button className="decrement " onClick={handlerest}>
-            
-            Reset
-          </button>
+    <div className='main'>
+    <h1 className='heading'>Counter App</h1>
+      <div className="main-card">
+        <div className="h1-part">
+          <h1>1</h1>
+        </div>
+        <div className="button-container">
+          <button className="green">Increment</button>
+          <button className="red">Decrement</button>
+        </div>
+        <div className='reset'>
+        <button className='reset-1'> Reset</button> 
         </div>
       </div>
     </div>
